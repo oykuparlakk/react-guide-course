2b4b582d9503d1d68f5531cc9b5050a1de341595
oykuparlak <oykuparlak2462@gmail.com>
2023-11-06 21:24:49 +0300

    integrate best practice

--- 01-starting-project/src/components/CoreConcept.jsx
+++ 01-starting-project/src/components/CoreConcept.jsx
@@ -1,10 +1,9 @@
-
-export function CoreConcept({image,title,description}){
-    return(
-        <li>
-            <img src={image} alt={title}/>
-            <h3>{title}</h3>
-            <p>{description}</p>
-        </li>
-    )
+export default function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
 }
