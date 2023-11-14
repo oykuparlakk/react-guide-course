f26e92d99f6d1b04e7c1ee7c9e3104a54fd45ff3
oykuparlak <oykuparlak2462@gmail.com>
2023-11-14 23:52:55 +0300

    form created

--- 03-investment-calculator/src/App.jsx
+++ 03-investment-calculator/src/App.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
 
 function App() {
-  return <Header />;
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((preUserInput) => {
+      return {
+        ...preUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
+    </>
+  );
 }
 
 export default App;
