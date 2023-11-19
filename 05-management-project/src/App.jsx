41436df86029096b38d04820b2fa5d0266c87449
oykuparlak <oykuparlak2462@gmail.com>
2023-11-19 17:24:39 +0300

    cruial components are created

--- 05-management-project/src/App.jsx
+++ 05-management-project/src/App.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import ProjectsSidebar from "./components/ProjectsSideBar";
+
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProject: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+      <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+    </main>
   );
 }
 
