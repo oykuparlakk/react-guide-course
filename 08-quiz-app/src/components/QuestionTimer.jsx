c798a9ef6951d3554b8c1c6d9981a4c58fd4fc34
oykuparlak <oykuparlak2462@gmail.com>
2023-11-25 23:24:44 +0300

    progress bar update styling with answer

--- 08-quiz-app/src/components/QuestionTimer.jsx
+++ 08-quiz-app/src/components/QuestionTimer.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 
-export default function QuestionTimer({ timeout, onTimeout }) {
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      onTimeout();
-    }, timeout);
+    const timer = setTimeout(onTimeout,timeout)
 
     return () => clearTimeout(timer);
   }, [onTimeout, timeout]);
@@ -19,5 +17,12 @@ export default function QuestionTimer({ timeout, onTimeout }) {
     return () => clearInterval(interval);
   }, []);
 
-  return <progress id="question-time" max={timeout} value={remainingTime} />;
+  return (
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={mode}
+    />
+  );
 }
