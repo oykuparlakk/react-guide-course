8e5422d174b8191a3355aaac2abccfaa6b3b3807
oykuparlak <oykuparlak2462@gmail.com>
2023-11-29 23:05:18 +0300

    All endpoints are connected

--- 10-data-fetching/src/http.js
+++ 10-data-fetching/src/http.js
@@ -6,3 +6,30 @@ export async function fetchAvailablePlaces() {
   }
   return resData.places;
 }
+
+export async function fetchUserPlaces() {
+  const response = await fetch("http://localhost:3000/user-places");
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error("Failed to fetch");
+  }
+  return resData.places;
+}
+
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({places}),
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+
+  const resData = response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to update user data");
+  }
+
+  return resData.message;
+}
